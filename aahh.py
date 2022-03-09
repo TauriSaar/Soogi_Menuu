122f8ed32fd675d9ccbe512df46728669b084fb3
TauriSaar <94620796+TauriSaar@users.noreply.github.com>
2022-03-09 11:03:11 +0200

    Update aahh.py

--- aahh.py
+++ aahh.py
@@ -1,22 +1,26 @@
+#Alguses impordin requests, json, BeautifulSoup ja Re
 import requests
 import json
 from bs4 import BeautifulSoup
 import re
 
+#toome firefox'i programi
 header = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
 
+#ühendame veebilehe
 base_url = "http://192.168.22.172/menu-example/"
 
+#taotleme veebilehelt infot
 r = requests.get(base_url,headers=header)
 
-
-
-
 if r.status_code == 200:
+    #loeb veebilehelt infot
     soup = BeautifulSoup(r.text, 'html.parser')
+    #leiab klassi
     books = soup.find_all('div',attrs={"class":"panel panel-default"})
+    #teeme result'i tuhja dictionary
     result=[]
-    
+    #teeme for loopi ja leiab kõik järgimed spetsiifilist infot
     for book in books:
         nimetus1 = book.find('h3').text
         print(nimetus1)
@@ -24,12 +28,11 @@ if r.status_code == 200:
         hind = book.contents[2].span.contents[0]
         lisainfo = book.find('small').text
         nimetus = book.find('h2').text
-        single ={'nimetus':nimetus1,'toidud':["nimetus: "+ toidud, "hind: "+ hind, "lisainfo: "+ lisainfo ], }
+        #ühendab leitud info tähendustega
+        single ={'nimetus':nimetus1,'toidud':[{'nimetus':toidud, 'hind':hind.replace("€", ""), 'lisainfo':lisainfo }], }
         result.append(single)
-    with open('söögimenüü.json','w', encoding="UTF-8") as f:
-        json.dump(result,f,indent=4)
+        #kirjutab kõik selle info Json faili
+        with open('söögimenüü.json','w', encoding="UTF-8") as f:
+            json.dump(result,f,indent=4)
 else:
   print(r.status_code)
-  
-  
-
